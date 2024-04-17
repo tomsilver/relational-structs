a1ab9d95ac30acc355cdb7376149fbd3834ccbcd
Tom Silver <tomsilver@users.noreply.github.com>
2024-04-17 15:09:48 -0400

    copy in basic structs from predicators  (#2)

--- src/relational_structs/structs.py
+++ src/relational_structs/structs.py
@@ -1,15 +1,195 @@
 """Data structures."""
 
-from dataclasses import dataclass
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from functools import cached_property
+from typing import Any, Dict, Iterator, List, Optional, Sequence
+
+import numpy as np
+from numpy.typing import NDArray
+from tabulate import tabulate
+from typing_extensions import TypeAlias
+
+Array: TypeAlias = NDArray[np.float32]
 
 
-@dataclass(frozen=True)
-class Dog:
-    """An example class."""
+@dataclass(frozen=True, order=True)
+class Type:
+    """Struct defining a type."""
 
     name: str
-    breed: str
+    feature_names: Sequence[str] = field(repr=False)
+    parent: Optional[Type] = field(default=None, repr=False)
 
-    def is_good(self) -> bool:
-        """All dogs are good."""
+    @property
+    def dim(self) -> int:
+        """Dimensionality of the feature vector of this object type."""
+        return len(self.feature_names)
+
+    def __call__(self, name: str) -> _TypedEntity:
+        """Convenience method for generating _TypedEntities."""
+        if name.startswith("?"):
+            return Variable(name, self)
+        return Object(name, self)
+
+    def __hash__(self) -> int:
+        return hash((self.name, tuple(self.feature_names)))
+
+
+@dataclass(frozen=True, order=True, repr=False)
+class _TypedEntity:
+    """Struct defining an entity with some type, either an object (e.g.,
+    block3) or a variable (e.g., ?block).
+
+    Should not be instantiated externally.
+    """
+
+    name: str
+    type: Type
+
+    @cached_property
+    def _str(self) -> str:
+        return f"{self.name}:{self.type.name}"
+
+    @cached_property
+    def _hash(self) -> int:
+        return hash(str(self))
+
+    def __str__(self) -> str:
+        return self._str
+
+    def __repr__(self) -> str:
+        return self._str
+
+    def is_instance(self, t: Type) -> bool:
+        """Return whether this entity is an instance of the given type, taking
+        hierarchical typing into account."""
+        cur_type: Optional[Type] = self.type
+        while cur_type is not None:
+            if cur_type == t:
+                return True
+            cur_type = cur_type.parent
+        return False
+
+
+@dataclass(frozen=True, order=True, repr=False)
+class Object(_TypedEntity):
+    """Struct defining an Object, which is just a _TypedEntity whose name does
+    not start with "?"."""
+
+    def __post_init__(self) -> None:
+        assert not self.name.startswith("?")
+
+    def __hash__(self) -> int:
+        # By default, the dataclass generates a new __hash__ method when
+        # frozen=True and eq=True, so we need to override it.
+        return self._hash
+
+
+@dataclass(frozen=True, order=True, repr=False)
+class Variable(_TypedEntity):
+    """Struct defining a Variable, which is just a _TypedEntity whose name
+    starts with "?"."""
+
+    def __post_init__(self) -> None:
+        assert self.name.startswith("?")
+
+    def __hash__(self) -> int:
+        # By default, the dataclass generates a new __hash__ method when
+        # frozen=True and eq=True, so we need to override it.
+        return self._hash
+
+
+@dataclass
+class State:
+    """Struct defining the low-level state of the world."""
+
+    data: Dict[Object, Array]
+    # Some environments will need to store additional simulator state, so
+    # this field is provided.
+    simulator_state: Optional[Any] = None
+
+    def __post_init__(self) -> None:
+        # Check feature vector dimensions.
+        for obj in self:
+            assert len(self[obj]) == obj.type.dim
+
+    def __iter__(self) -> Iterator[Object]:
+        """An iterator over the state's objects, in sorted order."""
+        return iter(sorted(self.data))
+
+    def __getitem__(self, key: Object) -> Array:
+        return self.data[key]
+
+    def get(self, obj: Object, feature_name: str) -> Any:
+        """Look up an object feature by name."""
+        idx = obj.type.feature_names.index(feature_name)
+        return self.data[obj][idx]
+
+    def set(self, obj: Object, feature_name: str, feature_val: Any) -> None:
+        """Set the value of an object feature by name."""
+        idx = obj.type.feature_names.index(feature_name)
+        self.data[obj][idx] = feature_val
+
+    def get_objects(self, object_type: Type) -> List[Object]:
+        """Return objects of the given type in the order of __iter__()."""
+        return [o for o in self if o.is_instance(object_type)]
+
+    def vec(self, objects: Sequence[Object]) -> Array:
+        """Concatenated vector of features for each of the objects in the given
+        ordered list."""
+        feats: List[Array] = []
+        if len(objects) == 0:
+            return np.zeros(0, dtype=np.float32)
+        for obj in objects:
+            feats.append(self[obj])
+        return np.hstack(feats)
+
+    def copy(self) -> State:
+        """Return a copy of this state.
+
+        The simulator state is assumed to be immutable.
+        """
+        new_data = {}
+        for obj in self:
+            new_data[obj] = self._copy_state_value(self.data[obj])
+        return State(new_data, simulator_state=self.simulator_state)
+
+    def _copy_state_value(self, val: Any) -> Any:
+        if val is None or isinstance(val, (float, bool, int, str)):
+            return val
+        if isinstance(val, (list, tuple, set)):
+            return type(val)(self._copy_state_value(v) for v in val)
+        assert hasattr(val, "copy")
+        return val.copy()
+
+    def allclose(self, other: State) -> bool:
+        """Return whether this state is close enough to another one, i.e., its
+        objects are the same, and the features are close."""
+        if self.simulator_state is not None or other.simulator_state is not None:
+            raise NotImplementedError(
+                "Cannot use allclose when simulator_state is not None."
+            )
+        if not sorted(self.data) == sorted(other.data):
+            return False
+        for obj in self.data:
+            if not np.allclose(self.data[obj], other.data[obj], atol=1e-3):
+                return False
         return True
+
+    def pretty_str(self) -> str:
+        """Display the state in a nice human-readable format."""
+        type_to_table: Dict[Type, List[List[str]]] = {}
+        for obj in self:
+            if obj.type not in type_to_table:
+                type_to_table[obj.type] = []
+            type_to_table[obj.type].append([obj.name] + list(map(str, self[obj])))
+        table_strs = []
+        for t in sorted(type_to_table):
+            headers = ["type: " + t.name] + list(t.feature_names)
+            table_strs.append(tabulate(type_to_table[t], headers=headers))
+        ll = max(len(line) for table in table_strs for line in table.split("\n"))
+        prefix = "#" * (ll // 2 - 3) + " STATE " + "#" * (ll - ll // 2 - 4) + "\n"
+        suffix = "\n" + "#" * ll + "\n"
+        return prefix + "\n\n".join(table_strs) + suffix
